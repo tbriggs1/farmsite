2b94e03700bac9e669dc4362f89a8c055f9ccc45
Thomas Briggs <57761163+tbriggs1@users.noreply.github.com>
2021-01-28 20:26:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-dfgdfgfd
+var burgerMenu = document.getElementById('burgerIcon');
+var burgerContent = document.getElementById('navMenu');
+var burgerIcons = document.getElementsByClassName('burgerButtons');
+
+burgerMenu.addEventListener('click', function(){
+    let getStyle = window.getComputedStyle(burgerContent);
+    if(getStyle.visibility === 'hidden'){
+        burgerContent.style.visibility = 'visible';
+        burgerIcons[0].classList.add('changeB1');
+        burgerIcons[1].classList.add('changeB2');
+        burgerIcons[2].classList.add('changeB3');
+    } else {
+        burgerContent.style.visibility = 'hidden';
+        burgerIcons[0].classList.remove('changeB1');
+        burgerIcons[1].classList.remove('changeB2');
+        burgerIcons[2].classList.remove('changeB3');
+    }
+})
+
+
+
+
+
